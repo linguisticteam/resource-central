c1f10eb056c0d3799997ccd343bfac9cbe6c3b79
Oxygen Barker <oxygenbarker@mail.com>
2015-02-07 21:08:06 +0100

    Update database.sql

--- sql/database.sql
+++ sql/database.sql
@@ -1,9 +1,9 @@
-START TRANSACTION
+START TRANSACTION;
 
-DROP DATABASE IF EXISTS `video_tutorials`
-CREATE DATABASE `video_tutorials`
+DROP DATABASE IF EXISTS `video_tutorials`;
+CREATE DATABASE `video_tutorials`;
 
-DROP TABLE IF EXISTS `video`
+DROP TABLE IF EXISTS `video`;
 CREATE TABLE `video` (
   `id` INT AUTO_INCREMENT,
   `author` VARCHAR(64),
@@ -13,7 +13,7 @@ CREATE TABLE `video` (
   PRIMARY KEY (id)
 ) ENGINE=MyISAM;
 
-DROP TABLE IF EXISTS `video_url`
+DROP TABLE IF EXISTS `video_url`;
 CREATE TABLE `video_url` (
   `id` INT AUTO_INCREMENT,
   `video_id` INT REFERENCES `video` (`id`),
