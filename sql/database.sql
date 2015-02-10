c5871603af6cce0b8b334ff9c381aa4bb55438ad
Oxygen Barker <oxygenbarker@mail.com>
2015-02-10 21:20:37 +0100

    Update database.sql

--- sql/database.sql
+++ sql/database.sql
@@ -58,6 +58,7 @@ INSERT INTO `creditee_type` (name) VALUES (
 
 CREATE TABLE `content` (
 	`id` INT AUTO_INCREMENT,
+	`parent_id` INT REFERENCES `content` (`id`),
 	`content_type_ref` INT REFERENCES `content_type` (`id`),
 	`content_info_ref` INT REFERENCES `content_info` (`id`),
 	`content_purpose_ref` INT REFERENCES `content_purpose` (`id`),
