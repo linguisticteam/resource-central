82872b0049fa52fe54c2efbd42b59fc0435e7048
Oxygen Barker <oxygenbarker@mail.com>
2015-02-10 18:49:41 +0100

    Update database.sql

--- sql/database.sql
+++ sql/database.sql
@@ -1,33 +1,25 @@
 START TRANSACTION;
 
-DROP DATABASE IF EXISTS `video_tutorials`;
-CREATE DATABASE `video_tutorials`
+DROP DATABASE IF EXISTS `tutorials`;
+CREATE DATABASE `tutorials`
 DEFAULT CHARACTER SET utf8
 DEFAULT COLLATE utf_unicode_ci;
 
-USE `video_tutorials`;
+USE `tutorials`;
 
-DROP TABLE IF EXISTS `video`;
-CREATE TABLE `video` (
-  `id` INT AUTO_INCREMENT,
-  `author` VARCHAR(64),
-  `title` VARCHAR(128),
-  `parts` INT UNSIGNED,
-  `description` MEDIUMTEXT,
-  PRIMARY KEY (id)
+CREATE TABLE `creditee` (
+	`id` INT AUTO_INCREMENT,
+	`full_name` TINYTEXT,
+	PRIMARY KEY (id)
 )
 ENGINE=MyISAM
 CHARACTER SET utf8
 COLLATE utf_unicode_ci;
 
-DROP TABLE IF EXISTS `video_url`;
-CREATE TABLE `video_url` (
-  `id` INT AUTO_INCREMENT,
-  `video_id` INT REFERENCES `video` (`id`),
-  `part` INT UNSIGNED,
-  `part_title` VARCHAR(128),
-  `url` VARCHAR(1024),
-  PRIMARY KEY (id)
+CREATE TABLE `creditee_type` (
+	`id` INT AUTO_INCREMENT,
+	`type` TINYTEXT,
+	PRIMARY KEY (id)
 )
 ENGINE=MyISAM
 CHARACTER SET utf8
