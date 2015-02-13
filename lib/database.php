daa37f1d9cea1c9b04e98df1f993c0e40c145b0a
oxygenbarker <oxygenbarker@mail.com>
2015-02-13 14:48:18 +0100

    Update database.php

--- lib/database.php
+++ lib/database.php
@@ -42,6 +42,39 @@ function db_connect() {
 //    add_part($connection,$title,$url,"NULL");
 //}
 
+function add_creditee($connection) {
+    $temp_id = mt_rand(-2^32/2,2^32/2-1);
+
+    $sql = "INSERT INTO creditee(temp_id)"
+        . "VALUES('{$temp_id}')";
+    $result = mysqli_query($connection, $sql);
+    if(!$result) {
+        // ToDo: output message probably through SESSION
+        echo "Could not enter data";
+        return;
+    }
+
+    $sql = "SELECT id FROM creditee WHERE temp_id LIKE '{$temp_id}'";
+    $result = mysqli_query($connection, $sql);
+    if(!$result) {
+        // ToDo: output message probably through SESSION
+        echo "Could not enter data";
+        return;
+    }
+
+    $creditee_id = mysqli_fetch_array($result)[0];
+
+    $sql = "UPDATE creditee SET temp_id = NULL WHERE id LIKE '{$creditee_id}'";
+    $result = mysqli_query($connection, $sql);
+    if(!$result) {
+        // ToDo: output message probably through SESSION
+        echo "Could not update table";
+        return;
+    }
+
+    return $creditee_id;
+}
+
 function add_creditee_attribute($connection, $creditee_id, $attribute_name, $attribute_value) {
     $sql = "SELECT id FROM creditee_attribute_type WHERE name LIKE '{$attribute_name}'";
     $result = mysqli_query($connection, $sql);
@@ -54,11 +87,10 @@ function add_creditee_attribute($connection, $creditee_id, $attribute_name, $att
     $creditee_attribute_type_id = mysqli_fetch_array($result)[0];
 
     $sql = "INSERT INTO creditee_attribute(creditee_id, creditee_attribute_type_id, value)"
-        . "VALUES('{$creditee_id}', '{$creditee_attribute_type_id}', '{$value}');"
+        . "VALUES('{$creditee_id}', '{$creditee_attribute_type_id}', '{$value}')";
     $result = mysqli_query($connection, $sql);
     if(!$result) {
         // ToDo: output message probably through SESSION
         echo "Could not enter data";
-        return;
     }
 }
