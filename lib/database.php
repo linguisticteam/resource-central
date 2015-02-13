e63fadc1317ec5cbd5b2295aab2f31a2ee12db1d
oxygenbarker <oxygenbarker@mail.com>
2015-02-13 11:13:08 +0100

    Update database.php

--- lib/database.php
+++ lib/database.php
@@ -12,30 +12,53 @@ function db_connect() {
     return $connection;
 }
 
-function get_video_id($title) {
-    $sql = "SELECT id"
-            . " FROM video"
-            . " WHERE title = '" . $title . "'";
-    $connection = db_connect();
+//OBSOLETE
+//
+//function get_video_id($title) {
+//    $sql = "SELECT id"
+//            . " FROM video"
+//            . " WHERE title = '" . $title . "'";
+//    $connection = db_connect();
+//    $result = mysqli_query($connection, $sql);
+//    if(!$result) {
+//        // ToDo: output message probably through SESSION
+//        echo "Could not read from video table";
+//    }
+//    $row = mysqli_fetch_array($result);
+//    return $row[0];
+//}
+//
+//function add_entry($connection, $author, $title, $url, $description) {
+//    $sql = "INSERT INTO video(author, title, parts, description)"
+//            . "VALUES('{$author}', '{$title}', NULL, '{$description}')";
+//    $result = mysqli_query($connection, $sql);
+//    if(!$result) {
+//        // ToDo: output message probably through SESSION
+//        echo "Could not write to video table";
+//    }
+//
+//    $video_id = get_video_id($title);
+//
+//    add_part($connection,$title,$url,"NULL");
+//}
+
+function add_creditee_attribute($connection, $creditee_id, $attribute_name, $attribute_value) {
+    $sql = "SELECT id FROM creditee_attribute_type WHERE name LIKE '{$attribute_name}'";
     $result = mysqli_query($connection, $sql);
     if(!$result) {
         // ToDo: output message probably through SESSION
-        echo "Could not read from video table";
+        echo "Could not retrieve data";
+        return;
     }
-    $row = mysqli_fetch_array($result);
-    return $row[0];
-}
 
-function add_entry($connection, $author, $title, $url, $description) {
-    $sql = "INSERT INTO video(author, title, parts, description)"
-            . "VALUES('{$author}', '{$title}', NULL, '{$description}')";
+    $creditee_attribute_type_id = $result;
+
+    $sql = "INSERT INTO creditee_attribute(creditee_id, creditee_attribute_type_id, value)"
+        . "VALUES('{$creditee_id}', '{$creditee_attribute_type_id}', '{$value}');"
     $result = mysqli_query($connection, $sql);
     if(!$result) {
         // ToDo: output message probably through SESSION
-        echo "Could not write to video table";
+        echo "Could not enter data";
+        return;
     }
-
-    $video_id = get_video_id($title);
-
-    add_part($connection,$title,$url,"NULL");
 }
