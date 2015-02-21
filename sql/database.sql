24cae3ee5dfba0af7afa7fc6951c5321a186d07f
Borislav Zlatanov <borislav_zlatanov@yahoo.co.uk>
2015-02-21 23:56:00 +0200

    Update database.sql
    Adding first inserts and a stored procedure to prove to ourselves that we CAN DO IT!!!

--- sql/database.sql
+++ sql/database.sql
@@ -79,6 +79,63 @@ INSERT INTO `element_type` (name) VALUES
 /*----   STORED PROCEDURES   ----*/
 /*-------------------------------*/
 
+/* Stored procedure to insert an entity */
+
+DELIMITER $$
+
+CREATE PROCEDURE insert_entity (IN param_element_title TEXT, IN param_entity_type_name TINYTEXT, IN param_entity_full_name TEXT)
+BEGIN
+    INSERT INTO `entity` (
+    resource_id,
+    entity_type_id,
+full_name
+    ) 
+VALUES (
+    (SELECT resource_id
+FROM element
+WHERE title = param_element_title),
+    (SELECT id
+FROM entity_type
+WHERE name = param_entity_type_name),
+    param_entity_full_name
+);
+
+END $$
+
+DELIMITER ;
+
+/* Make an entry into the resource table */
+INSERT INTO resource (
+    resource_type_id,
+    description
+)
+VALUES (
+(SELECT id
+FROM resource_type
+WHERE name = 'TUTORIAL'),
+'My description is tasty'
+);
+
+/* Make an entry into the element table */
+INSERT INTO element (
+    `resource_id`,
+    `element_type_id`,
+    `title`,
+    `index`,
+    `url`
+)
+VALUES (
+(SELECT MAX(id) 
+FROM resource),
+(SELECT id
+FROM element_type
+WHERE name = 'PRIMARY'),
+'How to database',
+NULL,
+'http://databaseieat.com'
+);
+
+CALL insert_entity('How to database','PERSON','My full name');
 
 
 COMMIT
