fec58fd18b9f18702e4db91bd8017e60f258bc00
Oxygen Barker <oxygenbarker@mail.com>
2015-03-11 04:23:18 +0100

    It whorx 8D

--- js/main.js
+++ js/main.js
@@ -13,42 +13,48 @@ $(function(){
 
         /* Declare local variables */
 
-        var iAdditionalAuthorCount;
-        var oAdditionalAuthors;
-        var oAuthorTemplateClone;
-        var sAuthorTypeNameAttribute;
-        var sNewAuthorTypeName;
-        var sNewResourceAuthorName;
-        var sResourceAuthorNameAttribute;
+        var author = {
+            clone: null,
+            attr: {
+                name_field_old_name: "",
+                name_field_new_name: "",
+                type_field_old_name: "",
+                type_field_new_name: ""
+            },
+            additional: {
+                object: null,
+                count: 0
+            }
+        };
 
         /* Initialize local variables */
 
         // (DOM) Get divs by id
-        oAdditionalAuthors = $("#additional_authors");
-        oAuthorTemplateClone = $("#author_template").clone();
+        author.additional.object = $("#additional_authors");
+        author.clone = $("#author_template").clone();
         
         /* Execute */
 
         // Get the Name attribute of the first author div
-        sResourceAuthorNameAttribute = oAuthorTemplateClone[0].children[AUTHOR_FIELD.NAME].name
-        sAuthorTypeNameAttribute = oAuthorTemplateClone[0].children[AUTHOR_FIELD.TYPE].name
+        author.attr.name_field_old_name = author.clone[0].children[AUTHOR_FIELD.NAME].name
+        author.attr.type_field_old_name = author.clone[0].children[AUTHOR_FIELD.TYPE].name
 
         // Get count of additional author divs already added
-        iAdditionalAuthorCount = oAdditionalAuthors.children().length;
+        author.additional.count = author.additional.object.children().length;
 
         // Create new name attribute based on how many additional authors already exist
-        sNewResourceAuthorName = IncrementNumeralInString(sResourceAuthorNameAttribute, 2, iAdditionalAuthorCount);
-        sNewAuthorTypeName = IncrementNumeralInString(sAuthorTypeNameAttribute, 1, iAdditionalAuthorCount);
+        author.attr.name_field_new_name = IncrementNumeralInString(author.attr.name_field_old_name, 2, author.additional.count);
+        author.attr.type_field_new_name = IncrementNumeralInString(author.attr.type_field_old_name, 1, author.additional.count);
 
         // Assign new name attribute to the clone
-        oAuthorTemplateClone[0].children[AUTHOR_FIELD.NAME].name = sNewResourceAuthorName;
-        oAuthorTemplateClone[0].children[AUTHOR_FIELD.TYPE].name = sNewAuthorTypeName;
+        author.clone[0].children[AUTHOR_FIELD.NAME].name = author.attr.name_field_new_name;
+        author.clone[0].children[AUTHOR_FIELD.TYPE].name = author.attr.type_field_new_name;
 
         // Remove the ID from the cloned div
-        oAuthorTemplateClone[0].removeAttribute("id");
+        author.clone[0].removeAttribute("id");
         
         // Insert clone into group of additional authors
-        oAdditionalAuthors.append(oAuthorTemplateClone);
+        author.additional.object.append(author.clone);
 
         /* Final */
 
