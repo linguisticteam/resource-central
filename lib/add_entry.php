2458eab434419e77ee715135f10b88a8d26c3f15
Oxygen Barker <oxygenbarker@mail.com>
2015-03-17 19:07:22 +0100

    Update add_entry.php

--- lib/add_entry.php
+++ lib/add_entry.php
@@ -2,13 +2,31 @@
 /* Don't allow direct access */
 //defined('START') or die();
 require_once('database.php');
+
+class FormProcessor {
+    private $author_type = null;
+    private $author_type_array = [];
+    private $resource_author = null;
+    private $resource_author_array = [];
+
+    public function __construct () { 
+    }
+
+    public function 
+
+    public static function isFieldPresent (string $Field) {
+        return !empty($_POST[$Field]);
+    }
+}
+
 if(!empty($_POST['title']) && !empty($_POST['resource_type']) && !empty($_POST['url']) && !empty($_POST['keywords']) && !empty($_POST['description'])) {
-    $resource_author_array = [];
-    $author_type_array = [];
+
+    /* Declare variables */
+
     for($i = 0; true; $i++) {
-        $resource_author_exists = !empty($_POST['resource_author_' . $i]);
-        $author_type_exists = !empty($_POST['author_' . $i . '_type']);
-        
+        $resource_author_exists = FormProcessor::isFieldPresent('resource_author_' . $i);
+        $author_type_exists = FormProcessor::isFieldPresent('author_' . $i . '_type');
+
         if( $resource_author_exists &&
             $author_type_exists) {
             // Both author name and author type exist.
