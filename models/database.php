fcadda3b661006c425524bdebbf344b43c0aac3e
Borislav Zlatanov <borislav_zlatanov@yahoo.co.uk>
2015-03-26 21:12:28 +0200

    Add method to get all types of something

--- models/database.php
+++ models/database.php
@@ -10,6 +10,27 @@ class Database extends mysqli {
     public function __construct() {
         parent::__construct(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
     }
+    
+    public function GetTypes($table_name, $column_name) {
+        $types_array = array();
+        
+        $sql = "SELECT {$column_name} FROM {$table_name}";
+        $result = $this->query($sql);
+        
+        if(!$result) {
+            Error::raise(__FILE__, __LINE__, 'GetTypesMethodFailed');
+            return;
+        }
+        
+        $total_rows = $result->num_rows;
+        
+        for($i = 0; $i < $total_rows; $i++) {
+            $row = $result->fetch_array(MYSQLI_NUM);
+            $types_array[] = $row[0];
+        }
+        
+        return $types_array;
+    }
 }
 
 $Database = new Database;
