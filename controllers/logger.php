6e1f22ad6c55b722686f8b43b6f1841feb30bfc8
Oxygen Barker <oxygenbarker@mail.com>
2015-04-06 19:37:36 +0200

    Added debug mode to logger

--- controllers/logger.php
+++ controllers/logger.php
@@ -10,6 +10,8 @@ class Logger {
 	private $info_prefix;  // Info
 	private $userr_prefix; // User error
 
+	private $debug_mode_active;
+
 	public function __construct() {
 
 		$this->log_name = "events.log";
@@ -18,6 +20,8 @@ class Logger {
 		$this->debug_prefix = "DEBUG: ";
 		$this->info_prefix  = " INFO: ";
 		$this->userr_prefix = "USERR: ";
+
+		$this->debug_mode_active = FALSE;
 	}
 
 	private function open() {
@@ -37,14 +41,32 @@ class Logger {
 		fwrite($this->file_pointer,$string_to_write);
 	}
 
+	public function activate_debug_mode() {
+
+		$this->debug_mode_active = TRUE;
+	}
+
+	public function deactivate_debug_mode() {
+
+		$this->debug_mode_active = FALSE;
+	}
+
+	public function is_debug_mode_active() {
+
+		return $this->debug_mode_active;
+	}
+
 	public function log_error($content_string) {
 		
 		$this->write($this->error_prefix,$content_string);
 	}
 
 	public function log_debug($content_string) {
-		
-		$this->write($this->debug_prefix,$content_string);
+
+		if($this->debug_mode_active) {
+
+			$this->write($this->debug_prefix,$content_string);
+		}
 	}
 
 	public function log_info($content_string) {
