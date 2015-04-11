2f90928bb5e03ebed39a30e75aa4eb4241c42082
Borislav Zlatanov <borislav_zlatanov@yahoo.co.uk>
2015-04-12 01:20:04 +0300

    Improved displaying of resources

--- index.php
+++ index.php
@@ -1,11 +1,23 @@
-<h1>Welcome to the Resource Central</h1>
-<p>This site has a collection of useful resources that we have gathered over the years</p>
-<?php
-require_once(dirname(__FILE__) . '/views/display_all_resources.php');
-?>
-<div id="resources_display">
-    <?php $ViewDisplayAllResources->DisplayAll(); ?>
-</div>
-<br>
-<div><a href="//github.com/linguisticteam/resource-central/" target="_blank">View this project on github</a></div>
-<div>Contact us</div>
+<html>
+    <head>
+        <title>Resource Central</title>
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+    </head>
+    
+    <body>
+    <h1>Welcome to the Resource Central</h1>
+    <p>This site has a collection of useful resources that we have gathered over the years</p>
+    <?php
+    require_once(dirname(__FILE__) . '/views/display_all_resources.php');
+    ?>
+    <div id="resources_display">
+        <?php $ViewDisplayAllResources->DisplayAll(); ?>
+    </div>
+    <br>
+    <div id="footer">
+        <span><a href="//github.com/linguisticteam/resource-central/" target="_blank">View this project on github</a></span>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <span>Contact us</span>
+    </div>
+    </body>
+</html>
--- views/display_all_resources.php
+++ views/display_all_resources.php
@@ -16,46 +16,36 @@ class ViewDisplayAllResources {
         $output = '';
         
         while($row = $result->fetch_array()) {
+            $output .= '<div class="resource">';
+            
             $title = htmlspecialchars($row['title']);
             $resource_type = htmlspecialchars($row['resource_type']);
             $description = htmlspecialchars($row['description']);
             $output .= "<h3>" . $title . "</h3>";
-            $output .= "<div>Type: " . ucwords(strtolower($resource_type));
-            $output .= ", Description: " . $description . "</div>";
+            $output .= "<table class='resource_info'><tbody><tr>Type: " . ucwords(strtolower($resource_type)) . '</tr>';
+            $output .= ' <tr>&nbsp;&nbsp;&nbsp;<a href="#" class="tooltip">'
+                    . '<img src="img/information-icon-small.png">'
+                    . '<span><img class="callout" src="img/callout_black.gif" />'
+                    . '<strong>Description</strong><br />' . $description
+                    . '</span></a></tr>';
             
-            
-            /* Get and display the keywords */
-            
-            $keywords_result = $this->Database->GetKeywords((int) $row['resource_id']);
-            $row_count = $keywords_result->num_rows;
-            
-            $output .= "<div>Keywords: ";
-            
-            for($i = 0; $i < $row_count; $i++) {
-                $keywords_row = $keywords_result->fetch_array();
-                $keyword = htmlspecialchars($keywords_row['keyword']);
-               
-                //Put a comma on every iteration after the first one
-                if($i != 0) {$output .= ", ";}
-                
-                $output .= $keyword;
-            }
-            
-            $output .= "</div>";
-        
-        
+                        
             /* Get and display URLs (this is to be expanded to more than just URLs in the future) */
 
             $urls_result = $this->Database->GetResourceURLs((int) $row['resource_id']);
 
-            $output .= "<div>URL: ";
+            $output .= "<tr>";
+            $output .= "&nbsp;&nbsp;&nbsp;";
 
             while($urls_row = $urls_result->fetch_array()) {
                 $url = htmlspecialchars($urls_row['url']);
-                $output .= $url;
+                $output .= '<a href="' . $url . ' target="_blank">See it!</a>';
             }
 
-            $output .= "</div>";
+            $output .= "</tr></tbody></table>";
+            
+            //Clear the float
+            $output .= '<div class="clear"></div>';
 
             
             /* Get and display the authors */
@@ -63,7 +53,7 @@ class ViewDisplayAllResources {
             $authors_result = $this->Database->GetAuthors((int) $row['resource_id']);
             $row_count = $authors_result->num_rows;
             
-            $output .= "<div>Author(s): ";
+            $output .= "<div class='authors'>Author(s): ";
             
             for($i = 0; $i < $row_count; $i++) {
                 $authors_row = $authors_result->fetch_array();
@@ -78,6 +68,28 @@ class ViewDisplayAllResources {
             
             $output .= "</div>";
             
+            
+            /* Get and display the keywords */
+            
+            $keywords_result = $this->Database->GetKeywords((int) $row['resource_id']);
+            $row_count = $keywords_result->num_rows;
+            
+            $output .= "<div class='keywords'>Keywords: ";
+            
+            for($i = 0; $i < $row_count; $i++) {
+                $keywords_row = $keywords_result->fetch_array();
+                $keyword = htmlspecialchars($keywords_row['keyword']);
+               
+                //Put a comma on every iteration after the first one
+                if($i != 0) {$output .= ", ";}
+                
+                $output .= $keyword;
+            }
+            
+            $output .= "</div>";
+            
+            
+            $output .= '</div>';  //close the class="resource" div
         }
         
         echo $output;
@@ -86,4 +98,3 @@ class ViewDisplayAllResources {
 
 $ViewDisplayAllResources = new ViewDisplayAllResources($Database);
 ?>
-
